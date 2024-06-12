7575b26fd8091dfc758f4fec72df058f7e93a766
Bhuwan Pandit <bp7968h@gre.ac.uk>
2024-06-12 21:24:16 +0100

    Add -c flag result

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,77 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::io::{Error};
 
 fn main(){
-    let args: Vec<String> = env::args().collect();
+    let args = get_arguments().unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Args is {:#?}", &args);
+    let config : FlagsFile = parse_arguments(&args[1..]);
 
-    if args.len() == 1 {
-        println!("No arguments supplied");
-    } else if args.len() == 2 {
-        println!("Error Format");
-    }else{
-        let config: Config = Config::build(&args);
-        println!("This is config: {:#?}", config);
-        display(config);
+    if config.file_name.is_empty() {
+        eprintln!("No file specified");
+        process::exit(1);
     }
+
+    let fileContent: String = read_file(&config.file_name).unwrap_or_else(|err| {
+        eprintln!("File Operation Error: {err}");
+        process::exit(1);
+    });
+
+    get_results(fileContent, &config.flags);
+}
+
+fn read_file(file: &String) -> Result<String, Error>{
+    fs::read_to_string(file)
+}
+
+fn get_arguments() -> Result<Vec<String>, &'static str>{
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        return Err("Not enough Parameters")
+    }
+    Ok(args)
 }
 
 #[derive(Debug)]
-struct Config<'a>{
-    filename: &'a String,
-    flags: Vec<&'a String>,
+struct FlagsFile {
+    file_name: String,
+    flags: Vec<String>,
 }
 
-impl<'a> Config<'a>{
-    fn build(&mut self,args: &'a [String]) -> Self{
-        // let mut flags: Vec<String> = Vec::new();
-        // let mut filename = String::new();
-        for arg in args{
-            if arg.starts_with("-"){
-                self.flags.push(&arg);
-            }else{
-                self.filename = &arg;
-            }
+fn parse_arguments(args: &[String]) -> FlagsFile {
+    let mut file_name: String = String::new();
+    let mut flags: Vec<String> = Vec::new();
+
+    for arg in args{
+        if arg.starts_with("-") {
+            flags.push(arg.clone());
+        }else {
+            file_name = arg.clone();
         }
-
-        self
     }
+
+    FlagsFile {file_name, flags}
 }
 
-fn display(config: Config){
-    for flag in config.flags{
-        if flag == "-c"{
-            let size = fs::metadata(config.filename.clone()).unwrap().len();
-            println!("Size is {:?}", size);
+
+struct FlagResults {
+    result: String
+}
+
+fn get_results(content: String, flags: &[String]) {
+    for flag in flags {
+        if flag == "-c" {
+            println!("Byte: {:?}", content.len());
+        }else if flag == "-l" {
+            println!("Line:");
+        }else if flag == "-w" {
+            println!("Words:");
+        }else if flag == "m" {
+            println!("Characters:");
         }
     }
 }
